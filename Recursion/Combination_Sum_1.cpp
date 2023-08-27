31b3902f5da84957b6176f1e8a9b2d8f61ea63f5
naren <91795163+narenpopale@users.noreply.github.com>
2023-08-27 22:34:23 +0530

    Create Combination_Sum_1.cpp

---
+++ Recursion/Combination_Sum_1.cpp
@@ -0,0 +1,58 @@
+// Given an array of distinct integers and a target, you have to return the list of all unique combinations where the chosen numbers sum to target. 
+// You may return the combinations in any order.
+
+// The same number may be chosen from the given array an unlimited number of times. 
+// Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+
+// TC - O(2^t * k) 
+// t - target
+// k - average length
+// SC - O(k*x)
+// k - average length
+// x - no. of combinations
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution {
+  public:
+    void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
+      if (ind == arr.size()) {
+        if (target == 0) {
+          ans.push_back(ds);
+        }
+        return;
+      }
+      // pick up the element 
+      if (arr[ind] <= target) {
+        ds.push_back(arr[ind]);
+        findCombination(ind, target - arr[ind], arr, ans, ds);
+        ds.pop_back();
+      }
+
+      findCombination(ind + 1, target, arr, ans, ds);
+
+    }
+  public:
+    vector < vector < int >> combinationSum(vector < int > & candidates, int target) {
+      vector < vector < int >> ans;
+      vector < int > ds;
+      findCombination(0, target, candidates, ans, ds);
+      return ans;
+    }
+};
+int main() {
+  Solution obj;
+  vector < int > v {2,3,6,7};
+  int target = 7;
+
+  vector < vector < int >> ans = obj.combinationSum(v, target);
+  cout << "Combinations are: " << endl;
+  for (int i = 0; i < ans.size(); i++) {
+    for (int j = 0; j < ans[i].size(); j++)
+      cout << ans[i][j] << " ";
+    cout << endl;
+  }
+}
