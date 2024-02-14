9e2aae7e4f6c6dd61b99ff3dbb8f44e2f01c218f
naren <narenpopale@gmail.com>
2024-02-14 12:22:33 +0530

    feat: another soln added

--- Greedy Algorithms/N_Meetings_In_One_Room.cpp
+++ Greedy Algorithms/N_Meetings_In_One_Room.cpp
@@ -6,6 +6,8 @@ using namespace std;
 class Solution
 {
 public:
+
+    // Solution for sorting it with respect to the start time of the meetings
     int maxMeetings(int start[], int end[], int n)
     {
         vector<pair<int, int>> v;
@@ -32,5 +34,29 @@ public:
         }
 
         return cnt;
+    }
+
+
+    // Solution for sorting it with respect to the end time of the meetings
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int, int>> v;
+        
+        for(int i = 0;i < n; i++ ) {
+            v.push_back({end[i], start[i]});
+        }
+        
+        sort(v.begin(), v.end());
+        int endTime = v[0].first;
+        int ans = 1;
+        
+        for(int i = 1; i < n; i++) {
+            if(endTime < v[i].second) {
+                endTime = v[i].first;
+                ans++;
+            }
+        }
+        
+        return ans;
     }
 };
